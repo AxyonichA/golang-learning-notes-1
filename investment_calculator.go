0d4235dbde3fca6f2ecb9921f4539020266b69d6
Alexandr <102229491+AxyonichA@users.noreply.github.com>
2025-06-29 16:00:01 +0500

    types and variables

---
+++ investment_calculator.go
@@ -0,0 +1,68 @@
+package main
+
+/* 
+import "fmt"
+import "math"
+^ можно объединить в один импорт >>
+*/
+import (
+	"fmt"
+	"math"
+)
+func main() {
+	// 1 Способ.
+	/*
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	важно соблюдать соответствие типов, участвующих в операции
+	float64 приводит к числу с плавающей запятой
+	var futureValue = float64(investmentAmount) * math.Pow(1 + expectedReturnRate / 100, float64(years))
+	*/
+	
+
+
+
+	// 2 Способ.
+	/*
+	так же можно сразу в объявлении задать тип переменной и объединить несколько объявлений в одно >>
+	если тип не указывается, то можно опустить ключевое слово var, используя := >>
+	*/
+	// var investmentAmount, years float64 = 1000, 10
+	// expectedReturnRate := 5.5 
+
+
+	// 3 Способ.
+	// ещё более простой путь - сразу присвоить переменным значения с плавающей запятой, однако это делает код менее читабельным >>
+	const inflationRate = 2.5
+	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount) // ожидаёт ввод в терминале
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate) // ожидаёт ввод в терминале
+
+	fmt.Print("Investment Horizon: ")
+	fmt.Scan(&years) // ожидаёт ввод в терминале
+	
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+}
+
+
+
+
+
+
+
+
+// go mod init example.com/investment-calculator - инициализирует модуль
+
+// go build - создает исполняемый файл
+
+// go run ./investment_calculator.go - запускает программу
